8df1446bfba7d3839d986724ab464a0b010b155c
dfwmtc18 <dfwmtc@gmail.com>
2018-07-23 22:51:09 -0500

    Search class is added and new method is added in class LoginPage

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,4 +4,8 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("This GIT test Loing Page");
 	}
+	
+	public void getWebUrl() {
+		System.out.println("New method getWebUrl is added");
+	}
 }
