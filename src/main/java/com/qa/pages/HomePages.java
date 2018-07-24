b56be03660a728d6c2d182d5af97f4a820557bcd
dfwmtc18 <dfwmtc@gmail.com>
2018-07-23 21:24:29 -0500

    first git commit for my Selenium code

---
+++ src/main/java/com/qa/pages/HomePages.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class HomePages {
+	public void homePage() {
+		System.out.println("This GIT Home Page.");
+	}
+}
---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class LoginPage {
+	public void loginPage() {
+		System.out.println("This GIT test Loing Page");
+	}
+}
